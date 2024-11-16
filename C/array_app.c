301d262ff7510359bffe300a2921ba041478ea8c
Mostafijul Islam <mostafijul1000@gmail.com>
2024-11-17 04:34:53 +0600

    chore: finished practice array app!

--- C/array_app.c
+++ C/array_app.c
@@ -1,3 +1,19 @@
+/*
+--- ---
+time: 04:30:48+06:00
+date: Sun 17, Nov 2024
+author: Mostafijul Islam
+problem: `N/A`
+
+*INPUT*
+- ???
+
+*QUESTION*
+- This is not a question. This is a practice problem. Where Im traversing the array and adding/removing the elements.
+  And it runs constantly until the user inputs 0. It's like a simple command-line application. It's just a practice problem.
+--- ---
+*/
+
 #include <stdio.h>
 
 // This function clears the console!
@@ -33,6 +49,7 @@ int main() {
     if (input == 1) {
       printf("Array status : \n");
       printf("length: %d\n", length);
+      printf("max_length: %d\n", initial_length);
       printf("items: *");
       for (int j = 0; j < length; j++) {
         printf("| %d |", array[j]);
@@ -59,17 +76,41 @@ int main() {
         array[pos - 1] = element;
         if (length < initial_length) {
           length++;
-        } else {
-          printf("You've entered an unsupported input!\nPlease pick one of the supported inputs!\n\n");
-          input = 1;
-          continue;
         }
 
         clear();
-        printf("Inserted value(%d) at position(%d)\n", element, pos);
+        printf("Inserted value(%d) at position(%d).\n\n", element, pos);
         input = 1;
         continue;
       }
+    } else if (input == 3) {
+      clear();
+      printf("Array Length: %d\n", length);
+      printf("Enter the position to remove: ");
+      int pos;
+      scanf("%d", &pos);
+      if (pos > initial_length || pos < 0) {
+        clear();
+        printf("Position out of bounds!\n\n");
+        input = 1;
+        continue;
+      } else {
+        int temp = array[pos - 1];
+        for (int k = pos - 1; k < length; k++) {
+          array[k] = array[k + 1];
+        }
+        length--;
+        clear();
+        printf("Removed value(%d) from position(%d).\n\n", temp, pos);
+        input = 1;
+        continue;
+      }
+
+    } else {
+      printf("You've entered an unsupported input!\n"
+             "Please pick one of the supported inputs!\n\n");
+      input = 1;
+      continue;
     }
 
     // Prompt user!
